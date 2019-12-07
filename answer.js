c805d9ca45d653801444fa426d9fffe36663236f
reeganford <rmford1695@gmail.com>
2019-12-06 23:35:00 -0700

    Finished challenge #5

--- answer.js
+++ answer.js
@@ -1,1 +1,19 @@
 // *** YOUR ANSWER BELOW ***
+
+let horiz = "-";
+let vert = "|";
+let space = " ";
+let corner = "+";
+
+function drawBox(x,y) {
+    let topRow = corner + horiz.repeat(x - 2) + corner;
+    console.log(topRow);
+    for(let i = 0; i < (y - 1); i++) {
+        if(i !== (y - 2)) {            
+            console.log(vert + space.repeat(x - 2) + vert);
+        } else {
+            console.log(topRow);
+        }
+    } 
+}
+(drawBox(20,10));
